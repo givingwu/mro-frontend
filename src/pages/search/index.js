20d9b5f36d8f8ca5c226513e3cc0775de98eff79
vuchan.c.wu <givingwu@gmail.com>
2019-02-19 18:13:21 +0800

    update scss and component

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,12 +1,7 @@
-import '../../assets/style/common.scss'
 import '../../assets/style/search.scss'
-
-import $ from 'jquery'
 import '../../includes/mixins/SearchTab'
 import '../../includes/mixins/CheckBox'
 import '../../includes/mixins/InputNumber'
 import '../../includes/mixins/PreviewSwitcher'
 import '../../includes/mixins/Tab'
 import '../../includes/mixins/SpecSelector'
-
-console.log($)
