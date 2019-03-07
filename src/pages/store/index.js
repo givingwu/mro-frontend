4ab3704740f73f49418d599cb846a310c0912f30
empty.Q.Wu <10419925@qq.com>
2019-03-07 09:40:49 +0800

    delete store-2

--- src/pages/store/index.js
+++ src/pages/store/index.js
@@ -9,6 +9,26 @@ window.$ = $
 
 $(function () {
   $('.J_SideBar').initSideBar()
+
+  var infoBox = null
+  $('.J_ShowProjectInfo ul li').hover(function () {
+    var top = $(this).offset().top
+    var left = $(this).offset().left
+    var infoText = $(this).find('span').text()
+    var spanWidth = $(this).find('span').width()
+    if ($(this).width() < spanWidth) {
+      infoBox = '<div class="project-info-box">' + infoText + '</div>'
+      $('body').append(infoBox)
+      $('.project-info-box').css({
+        'left': left + 'px',
+        'top': top - $(window).scrollTop() + 36 + 'px'
+      }).addClass('show')
+    }
+  }, function () {
+    if (infoBox) {
+      $('.project-info-box').remove()
+    }
+  })
   $('.J_ShowMore').click(function () {
     var self = $(this)
     var $listDom = self.parent().find('.project-list')
