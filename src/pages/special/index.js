b6468c1b10f07c3d4bf306bead62563285cf7abe
empty.Q.Wu <10419925@qq.com>
2019-03-07 15:49:05 +0800

    fix

--- src/pages/special/index.js
+++ src/pages/special/index.js
@@ -3,3 +3,12 @@ import $ from 'jquery'
 import '../../common'
 
 window.$ = $
+
+$(function () {
+  $('.J_SpecialTab ul li').click(function () {
+    var index = $(this).index()
+    console.log($(this))
+    $(this).addClass('active').siblings().removeClass('active')
+    $('.J_SpecialContent').find('.J_SpecialContentList').eq(index).show().siblings().hide()
+  })
+})
